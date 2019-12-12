a5f7539b31fb5d27ee1d214da3229fe94d3a7668
karta2164480 <karta2164480@github.com>
2019-12-12 14:45:01 +0800

    Delete the change in main.py

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ mass = {'sun': 1.989E30,'earth': 5.972E24, 'mars':6.4169E23, 'halley':2.2E14}
 d_at_perihelion = {'earth': 1.495E11, 'mars':2.279E11, 'halley': 8.7665E10}
 v_at_perihelion = {'earth': 2.9783E4, 'mars':2.4077E4, 'halley': 54563.3}
 
-def G_force(M,m, pos_vec):
-    return -G * M * m / mag2(pos_vec) * norm(pos_vec)
+def G_force(m, pos_vec):
+    return -G * mass['sun'] * m / mag2(pos_vec) * norm(pos_vec)
 
 class as_obj(sphere):
     def kinetic_energy(self):
         return 0.5 * self.m * mag2(self.v)
     def potential_energy(self):
         return - G * mass['sun'] * self.m / mag(self.pos)
-
 scene = canvas(width=800, height=800, background=vector(0.5,0.5,0))
 scene.lights = []
 sun = sphere(pos=vector(0,0,0), radius = 3.0E10, color = color.orange, emissive=True)
@@ -23,11 +22,9 @@ mars = as_obj(pos = vector(d_at_perihelion['mars'],0,0), radius = 1.0E10, m = ma
 mars.v = vector(0, 0, - v_at_perihelion['mars'])
 halley = as_obj(pos = vector(d_at_perihelion['halley'],0,0), radius = 0.5E10, m = mass['halley'], make_trail = True)
 halley.v = vector(0, 0, - v_at_perihelion['halley'])
-
 stars = [earth, mars, halley]
 dt=60*60*6
 print(earth.potential_energy(), earth.kinetic_energy())
-
 while True:
     rate(1000)
     for star in stars:
