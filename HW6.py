8990872441d7885080d0bc04eda9d7f5c45563a1
karta2164480 <karta2164480@github.com>
2019-12-12 09:53:38 +0800

    Let the Moon orbit the Earth

--- HW6.py
+++ HW6.py
@@ -9,7 +9,7 @@ moon_orbit = {'r': 3.84E8, 'v': 1.022E3}
 theta = 5.145*pi/180.0
 
 def G_force(m, pos_vec):
-    return -G * mass['sun'] * m / mag2(pos_vec) * norm(pos_vec)
+    return -G * mass['earth'] * m / mag2(pos_vec) * norm(pos_vec)
 
 class as_obj(sphere):
     def kinetic_energy(self):
@@ -22,24 +22,25 @@ scene.lights = []
 
 #sun = sphere(pos=vector(0,0,0), radius = radius['sun'], color = color.orange, emissive=True)
 
-scene.forward = vector(0, -1, 0)
+#scene.forward = vector(0, -1, 0)
 
 local_light(pos=vector(0,0,0))
-earth = as_obj(pos = vector(0,0,0), radius = radius['earth'], m = mass['earth'], texture={'file':textures.earth}, make_trail = True)
+earth = as_obj(pos = vector(0,0,0), radius = radius['earth'], m = mass['earth'], texture={'file':textures.earth}, make_trail = False)
 
 moon = as_obj(pos = vector(moon_orbit['r'], 0, 0), radius = radius['moon'], m = mass['moon'], v = vector(0, 0, -moon_orbit['v']))
 
 
-
+stars = [moon]
+dt=60
 '''
 stars = [earth, mars, halley]
 dt=60*60*6
 print(earth.potential_energy(), earth.kinetic_energy())
-
+'''
 while True:
     rate(1000)
     for star in stars:
         star.a = G_force(star.m, star.pos) / star.m
         star.v = star.v + star.a * dt
         star.pos = star.pos + star.v * dt
-'''
+
